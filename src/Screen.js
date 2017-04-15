76edd59e8fbee85548eae6b58fc346fd34bfa7c9
Amanda Hinchey <akhinchey@gmail.com>
2017-04-15 19:45:03 -0400

    Make API call, output still needs to be rendered.

--- src/Screen.js
+++ src/Screen.js
@@ -3,10 +3,25 @@ import Search from './Search';
 import Result from './Result'
 
 class Screen extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movieList: [],
+    };
+  }
+
+  updateMovies(movies) {
+    this.setState({
+      movieList: movies
+    })
+    console.log(this.state.movieList);
+  }
+
   render() {
     return (
       <div id="main">
-        <Search />
+        <Search updateMovies={this.updateMovies.bind(this)} />
         <Result />
       </div>
       );
